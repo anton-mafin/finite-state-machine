b2f8dcb1c11cecff840b69419b5399962c7f6221
anton <prozor.anton@gmail.com>
2017-09-29 21:00:07 +0300

    first

--- src/fsm.js
+++ src/fsm.js
@@ -3,28 +3,63 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+         if(config) {
+            this.config = config;  
+            this.initial = config.initial;
+            this.current = this.initial;
+            this.history = [this.initial];   
+            this.l = 0; 
+        }
+       else 
+            throw new Error();
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.current;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+       var flag = false;
+        for (let st in this.config.states)
+           if (st === state) {
+               this.current = state;
+               this.history.push(this.current);
+               flag = true;
+               this.localStorage = [];
+           }
+        if (!flag)
+            throw new Error();
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        var flag = false;  
+        for (let st in this.config.states[this.current])
+            for (let tr in this.config.states[this.current].transitions)
+                if (tr === event){
+                    this.current = this.config.states[this.current].transitions[tr];
+                    this.history.push(this.current);
+                    flag = true;
+                    this.localStorage = [];
+                }
+        if (!flag)
+            throw new Error();
+    }
 
     /**
-     * Resets FSM state to initial.
+     * Resets FSM state to start.
      */
     reset() {}
 
@@ -51,9 +86,9 @@ class FSM {
     redo() {}
 
     /**
-     * Clears transition history
+     * Clears transition h
      */
-    clearHistory() {}
+    clearh() {}
 }
 
 module.exports = FSM;
